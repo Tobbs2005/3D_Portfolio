f6e485c4a864c79099cc6ba84d7be8d74af33f65
Toby Fang <tobyfang05@gmail.com>
2025-07-16 09:39:28 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,56 @@
+import "./style.scss";
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
+const canvas = document.querySelector("#experience-canvas");
+
+
+const scene = new THREE.Scene();
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight
+}
+
+const camera = new THREE.PerspectiveCamera( 75, sizes.width/sizes.height, 0.1, 1000 );
+camera.position.z = 5;
+
+
+
+const renderer = new THREE.WebGLRenderer({canvas: canvas, antialias: true});
+renderer.setSize( sizes.width, sizes.height );
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+const controls = new OrbitControls( camera, renderer.domElement );
+controls.enableDamping = true;
+controls.dampingFactor = 0.05;
+controls.update();
+
+// Event Listeners
+window.addEventListener("resize", ()=> {
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  //Update Camera
+  camera.aspect = sizes.width/sizes.height
+  camera.updateProjectionMatrix()
+
+  //update render
+  renderer.setSize( sizes.width, sizes.height );
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+
+const render = () => {
+  controls.update();
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+
+  renderer.render( scene, camera );
+  window.requestAnimationFrame(render)
+}
+render();
